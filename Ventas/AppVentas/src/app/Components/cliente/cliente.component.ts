3a2c5e4f30171b3746eedd3e9aa84bfa72a88409
AlexisLuna <alexisgabriel.lu@gmail.com>
2024-08-12 16:09:34 -0500

    add tabla de clientes

--- Ventas/AppVentas/src/app/Components/cliente/cliente.component.ts
+++ Ventas/AppVentas/src/app/Components/cliente/cliente.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Respuesta } from '../../Models/Respuesta';
+import { Observable } from 'rxjs';
+
+import { ApiclienteService } from '../../Services/apicliente.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-cliente',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './cliente.component.html',
   styleUrl: './cliente.component.scss'
 })
 export class ClienteComponent {
-
+  public lst: any[] = [];
+  public columnas: string[] = ["id", "nombre"];
+  constructor(
+    private apiCliente: ApiclienteService
+  ) {
+    apiCliente.getClientes().subscribe(respuesta => {
+      this.lst = respuesta.data;
+      console.log(respuesta);
+    })
+  }
+  getCliente() {
+    /* this.apiCliente.getClientes().subscribe(respuesta => {
+      this.lst = respuesta.data;
+      console.log(respuesta);
+    }) */
+  }
 }
--- Ventas/AppVentas/src/app/Services/apicliente.service.ts
+++ Ventas/AppVentas/src/app/Services/apicliente.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Respuesta } from '../Models/Respuesta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiclienteService {
 
-  constructor() { }
+  private url: string = "https://localhost:7226/api/Cliente";
+
+
+  //COnstructor
+  private _http = inject(HttpClient);
+  constructor(/* private _http: HttpClient */) {
+
+  }
+
+  /* getCliente(): Observable<Respuesta>
+  {
+    return this.http.get<Respuesta>(this._url);
+  } */
+  getClientes(): Observable<Respuesta> {
+    return this._http.get<Respuesta>(this.url);
+
+  }
 }
