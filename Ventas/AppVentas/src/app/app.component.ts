1f7901140d781fa2e4a0a9cc94709e45e16b076a
AlexisLuna <alexisgabriel.lu@gmail.com>
2024-08-12 14:11:29 -0500

    add component  & interface respuesta

--- Ventas/AppVentas/src/app/app.component.ts
+++ Ventas/AppVentas/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from "@angular/material/card";
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, MatSidenavModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Ventas/AppVentas/src/app/app.routes.ts
+++ Ventas/AppVentas/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ClienteComponent } from './Components/cliente/cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: HomeComponent },
+    { path: "", redirectTo: 'home', pathMatch: "full" },
+    { path: "cliente", component: ClienteComponent }
+];
+
